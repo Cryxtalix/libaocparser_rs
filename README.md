428b073b1299a30f35b1bff064025db7b0550542
cryxtalix <fgx8p9t@protonmail.com>
2025-11-29 02:07:29 +0800

    Added readme

---
+++ README.md
@@ -0,0 +1,56 @@
+# Libaocparser_rs
+An AOC input parser for Rust
+
+## Install
+
+
+## Usage
+Use `AocParser::new` to add input file.
+
+`AocParser::slice_as_type` to convert specific lines into a particular type.
+
+Example
+
+```
+use libaocparser_rs::AocParser;
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct TestType {
+    x: u32,
+    y: u32,
+    z: u32,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct TestErr;
+
+impl FromStr for TestType {
+    type Err = TestErr;
+
+    fn from_str(s: &str) -> Result<Self, TestErr> {
+        let mut parts = s.split_whitespace();
+
+        let x = parts.next().ok_or(TestErr)?.parse::<u32>().map_err(|_| TestErr)?;
+        let y = parts.next().ok_or(TestErr)?.parse::<u32>().map_err(|_| TestErr)?;
+        let z = parts.next().ok_or(TestErr)?.parse::<u32>().map_err(|_| TestErr)?;
+        //                     ^ next part is None
+        //                                                     ^ parsing failed
+
+        if parts.next().is_some() {
+            // More parts than expected!
+            return Err(TestErr)
+        }
+
+        Ok(TestType { x, y, z })
+    }
+}
+
+fn main() {
+    let aoc = AocParser::new("inputs/day1/input.txt").unwrap();
+    let test: Vec<TestType> = aoc.slice_as_type(Some(3), Some(6)).unwrap();
+
+    assert_eq!(aoc.size, 250);
+    assert_eq!(test.len(), 3);
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -15,25 +15,43 @@ use std::{
 use errors::AocError;
 
 pub struct AocParser {
+    original: String,
     pub data: Vec<String>,
+    pub size: usize,
 }
 
 impl AocParser {
+    /// Path to AOC input file
+    /// Path starts at the project root
     pub fn new<P: AsRef<Path>>(path: P) -> Result<Self, std::io::Error> {
         let contents = fs::read_to_string(path)?;
-        let data = contents
+        let data: Vec<String> = contents
             .lines()
             .map(String::from)
             .collect();
+        let size = data.len();
         Ok(Self {
+            original: contents,
             data,
+            size,
         })
     }
 
+    /// Consumes Self and returns Vec<String>
+    pub fn get(self) -> Vec<String> {
+        self.data
+    }
+
+    /// Consumes Self and returns unformatted String
+    pub fn get_str(self) -> String {
+        self.original
+    }
+
+    /// Converts several lines into a specific type
     pub fn slice_as_type<T: FromStr>(&self, start: Option<usize>, end: Option<usize>) -> Result<Vec<T>, AocError> {
         let start = start.unwrap_or(0);
-        let end = end.unwrap_or(self.data.len());
-        if start > end || end > self.data.len() {
+        let end = end.unwrap_or(self.size);
+        if start > end || end > self.size {
             return Err(AocError::OutOfBounds)
         }
         self.data[start..end]
@@ -60,6 +78,7 @@ mod tests {
     #[test]
     fn test_slice_as_type() {
         let input = AocParser {
+            original: "test".to_string(),
             data: vec![
                 "1 2".to_string(),
                 "5 6".to_string(),
@@ -68,6 +87,7 @@ mod tests {
                 "7442 78524".to_string(),
                 "4210 7524".to_string(),
             ],
+            size: 6,
         };
         let out: Vec<String> = input.slice_as_type(Some(1), Some(5)).unwrap();
         assert_eq!(out.len(), 4);
