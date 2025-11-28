5382f0547f019b5dbe5fdde7e4f3d6b2a26130ab
cryxtalix <fgx8p9t@protonmail.com>
2025-11-29 01:18:09 +0800

    Added license header

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+/* This file is part of libaocparser_rs.
+
+libaocparser_rs is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+libaocparser_rs is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with MMWP. If not, see <https://www.gnu.org/licenses/>. */
+
 mod errors;
 use std::{
     fs,
